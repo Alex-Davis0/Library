c1a9ec113f41ca1e031b903f580bc5a7c631e1a3
Alex-Davis0 <amchops@comcast.net>
2022-11-22 15:58:11 -0800

    setting up parse routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
   render() {
     return <Home />;
   }
