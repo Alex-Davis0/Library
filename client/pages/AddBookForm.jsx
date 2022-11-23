86bcc4a23b2c9381c240135dc50c1dd4ccf9a65c
Alex-Davis0 <amchops@comcast.net>
2022-11-22 18:14:07 -0800

    started setup of form

--- client/pages/AddBookForm.jsx
+++ client/pages/AddBookForm.jsx
@@ -3,8 +3,26 @@ import Nav from '../components/Nav';
 
 export default function Home(props) {
   return (
+    <>
     <div>
       <Nav />
     </div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+            <div id="emailHelp" className="form-text"></div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1"></input>
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </>
   );
 }
