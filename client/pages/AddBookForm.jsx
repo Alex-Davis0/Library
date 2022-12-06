855de488afa627ba679e92f0e918b62ff1d9c373
Alex-Davis0 <amchops@comcast.net>
2022-12-05 16:15:27 -0800

    finished rough form

--- client/pages/AddBookForm.jsx
+++ client/pages/AddBookForm.jsx
@@ -7,21 +7,17 @@ export default function Home(props) {
     <div>
       <Nav />
     </div>
-      <form>
+      <form className='top-spacing'>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-            <div id="emailHelp" className="form-text"></div>
+          <label className="form-label text-col">Book Name</label>
+          <input className="form-control" aria-describedby="emailHelp"></input>
+            <div className="form-text"></div>
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1"></input>
+          <label className="form-label text-col">Author</label>
+          <input className="form-control"></input>
         </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary add-col">Submit</button>
       </form>
     </>
   );
